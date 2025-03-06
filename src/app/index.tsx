609cad4ccd6f1271d6f3b709783c6c1754fbe752
Sergio Castro <sergiojuniorce@gmail.com>
2025-03-05 23:18:42 -0700

    advanced todo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,15 +7,45 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import { useRef, useState } from 'react';
+
+import { useState } from 'react';
+
+type TodoCategory = {
+  id: string;
+  name: string;
+};
+
+type Todo = {
+  id: string;
+  text: string;
+  category: TodoCategory;
+  deleted: boolean;
+  archived: boolean;
+  createdAt?: Date;
+  completedAt: Date | null;
+};
 
 export default function Index() {
-  const [todos, setTodos] = useState<string[]>([]);
-  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState<Todo>({
+    id: '',
+    text: '',
+    category: { id: '', name: '' },
+    deleted: false,
+    archived: false,
+    completedAt: null,
+  });
 
   const handleAddTodo = () => {
     setTodos([...todos, todo]);
-    setTodo('');
+    setTodo({
+      id: '',
+      text: '',
+      category: { id: '', name: '' },
+      deleted: false,
+      archived: false,
+      completedAt: null,
+    });
   };
 
   const handleClearTodos = () => {
@@ -23,30 +53,60 @@ export default function Index() {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <TextInput placeholder="Add a todo" value={todo} onChangeText={setTodo} />
-      <Pressable
-        onPress={handleAddTodo}
-        style={{ backgroundColor: 'black', padding: 10, borderRadius: 5 }}
-      >
-        <Text className="text-white">Add</Text>
-      </Pressable>
-      <Pressable onPress={handleClearTodos} style={{ backgroundColor: 'red', padding: 10, borderRadius: 5 }}>
-        <Text className="text-white">Clear</Text>
-      </Pressable>
+    <View className="m-3">
+      <View className="flex flex-row gap-2">
+        <TextInput
+          className="flex-1 border border-gray-300 rounded-md p-2"
+          placeholder="Add a todo"
+          value={todo.text}
+          onChangeText={(text) => setTodo({ ...todo, text })}
+        />
+        <Pressable
+          onPress={handleAddTodo}
+          className="bg-green-600 rounded-md flex items-center justify-center w-20"
+        >
+          <Text className="text-white">Add</Text>
+        </Pressable>
+        <Pressable
+          onPress={handleClearTodos}
+          className="bg-red-600 rounded-md flex items-center justify-center w-20"
+        >
+          <Text className="text-white">Clear</Text>
+        </Pressable>
+      </View>
 
       <FlatList
         data={todos}
-        renderItem={({ item }) => (
-          <Text className="text-2xl">{item}</Text>
-        )}
+        renderItem={({ item }) => <TodoItem todo={item} />}
       />
     </View>
   );
 }
+
+const TodoCategoryDropdown = () => {
+  return (
+    <View>
+      <Text>Todo Category</Text>
+    </View>
+  );
+};
+
+const TodoCategoryItem = ({ category }: { category: TodoCategory }) => {
+  return <Text>{category.name}</Text>;
+};
+
+const TodoItem = ({ todo }: { todo: Todo }) => {
+  return (
+    <View className="flex flex-row gap-2">
+      <Text className="text-2xl">{todo.text}</Text>
+      <Pressable
+        className="bg-red-600 rounded-md flex items-center justify-center w-20"
+        onPress={() => {
+          console.log('delete');
+        }}
+      >
+        <Text className="text-white">Delete</Text>
+      </Pressable>
+    </View>
+  );
+};
